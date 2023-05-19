4ccb448b12134ade0dd4ca96374e4a4cb2e04eeb
MatPrina <132628938+MatPrina@users.noreply.github.com>
2023-05-19 19:18:35 -0300

    Update ConnectionFactory.java
    Falta Adicionar TimeZone

--- Quiz/src/main/java/com/mycompany/quiz/ConnectionFactory.java
+++ Quiz/src/main/java/com/mycompany/quiz/ConnectionFactory.java
@@ -8,6 +8,23 @@ package com.mycompany.quiz;
  *
  * @author mathe
  */
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class ConnectionFactory {
+    private String usuario = "root";
+    private String senha = "prina71";
+    private String host = "localhost";
+    private String porta = "3306";
+    private String bd = "db_quiz";
     
+    public Connection obtemConexao(){
+        try{
+            Connection c = DriverManager.getConnection("jdbc:mysql://"+host+":"+porta+"/"+bd,usuario,senha);
+            return c;
+        }catch(Excpetion e){
+            e.printStackTrace();
+            return null;
+        } 
+    }
 }
